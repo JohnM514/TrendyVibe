0371f11a3c4db02b2f173f40bc6cada00640f866
JohnM514 <juandedios.carranzam@gmail.com>
2024-12-07 22:08:24 -0600

    Created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Badge from "@mui/material/Badge";
+import { grey } from "@mui/material/colors";
+
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  align-items: center;
+  display: flex;
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Language = styled.span`
+  font-size: 14;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  align-items: center;
+  display: flex;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon sx={{ color: "gray", fontSize: 14}} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>TrendyVibe</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge>
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
